5f9725dfb2d8f763b38339fafbfc88b7dc614f2d
mmyleung <63979415+mmyleung@users.noreply.github.com>
2022-10-23 19:43:55 +0100

    Buttons work on js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('javascript is working!');
+document.getElementById("button-1").addEventListener("click", function() {
+    console.log("button 1 targeted");
+});
+document.getElementById("button-2").addEventListener("click", function() {
+    console.log("button 2 targeted");
+});
+document.getElementById("button-3").addEventListener("click", function() {
+    console.log("button 3 targeted");
+});
+document.getElementById("button-4").addEventListener("click", function() {
+    console.log("button 4 targeted");
+});
+document.getElementById("button-5").addEventListener("click", function() {
+    console.log("button 5 targeted");
+});
+document.getElementById("button-6").addEventListener("click", function() {
+    console.log("button 6 targeted");
+});
