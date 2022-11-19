1a83e250282c769fdf8d661fa9468ab8faa67382
mmyleung <63979415+mmyleung@users.noreply.github.com>
2022-11-19 15:27:44 +0800

    change text to show less when description box is shown

--- script.js
+++ script.js
@@ -2,80 +2,105 @@ console.log('javascript is working!');
 
 const descriptionOne = (document.getElementById('description-1'));
 descriptionOne.style.display = 'none';
-document.getElementById('button-1').addEventListener('click', function() {
+const btn1 = document.getElementById('button-1');
+btn1.addEventListener('click', function() {
    if (descriptionOne.style.display === 'none') {
     descriptionOne.style.display = 'block'; 
+    btn1.innerHTML = "Show less";
+    
    } else {
         descriptionOne.style.display = 'none' ;
+        btn1.innerHTML = "Find out more!";
     }
 });
 
 const descriptionTwo = (document.getElementById('description-2'))
 descriptionTwo.style.display = 'none';
-document.getElementById('button-2').addEventListener('click', function() {
+const btn2 = document.getElementById('button-2');
+btn2.addEventListener('click', function() {
     if (descriptionTwo.style.display === 'none') {
         descriptionTwo.style.display = 'block'; 
+        btn2.innerHTML = "Show less";
        } else {
             descriptionTwo.style.display = 'none' ;
+            btn2.innerHTML = "Find out more!";
         }
 });
 
 const descriptionThree = (document.getElementById('description-3'))
 descriptionThree.style.display = 'none';
-document.getElementById('button-3').addEventListener('click', function() {
+const btn3 = document.getElementById('button-3');
+btn3.addEventListener('click', function() {
     if (descriptionThree.style.display === 'none') {
         descriptionThree.style.display = 'block'; 
+        btn3.innerHTML = "Show less";
        } else {
             descriptionThree.style.display = 'none' ;
+            btn3.innerHTML = "Find out more!";
         }
 });
 
 const descriptionFour = (document.getElementById('description-4'))
 descriptionFour.style.display = 'none';
-document.getElementById('button-4').addEventListener('click', function() {
+const btn4 = document.getElementById('button-4');
+btn4.addEventListener('click', function() {
     if (descriptionFour.style.display === 'none') {
         descriptionFour.style.display = 'block'; 
+        btn4.innerHTML = "Show less";
        } else {
             descriptionFour.style.display = 'none' ;
+            btn4.innerHTML = "Find out more!";
         }
 });
 
 const descriptionFive = (document.getElementById('description-5'))
 descriptionFive.style.display = 'none';
-document.getElementById('button-5').addEventListener('click', function() {
+const btn5 = document.getElementById('button-5');
+btn5.addEventListener('click', function() {
     if (descriptionFive.style.display === 'none') {
         descriptionFive.style.display = 'block'; 
+        btn5.innerHTML = "Show less";
        } else {
             descriptionFive.style.display = 'none' ;
+            btn5.innerHTML = "Find out more!";
         }
 });
 
 const descriptionSix = (document.getElementById('description-6'))
 descriptionSix.style.display = 'none';
-document.getElementById('button-6').addEventListener('click', function() {
+const btn6 = document.getElementById('button-6');
+btn6.addEventListener('click', function() {
     if (descriptionSix.style.display === 'none') {
         descriptionSix.style.display = 'block'; 
+        btn6.innerHTML = "Show less";
        } else {
             descriptionSix.style.display = 'none' ;
+            btn6.innerHTML = "Find out more!";
         }
 });
 
 const descriptionSeven = (document.getElementById('description-7'))
 descriptionSeven.style.display = 'none';
-document.getElementById('button-7').addEventListener('click', function() {
+const btn7 = document.getElementById('button-7');
+btn7.addEventListener('click', function() {
     if (descriptionSeven.style.display === 'none') {
         descriptionSeven.style.display = 'block'; 
+        btn7.innerHTML = "Show less";
        } else {
             descriptionSeven.style.display = 'none' ;
+            btn7.innerHTML = "Find out more!";
         }
 });
 
 const descriptionEight = (document.getElementById('description-8'))
 descriptionEight.style.display = 'none';
-document.getElementById('button-8').addEventListener('click', function() {
+const btn8 = document.getElementById('button-8');
+btn8.addEventListener('click', function() {
     if (descriptionEight.style.display === 'none') {
         descriptionEight.style.display = 'block'; 
+        btn8.innerHTML = "Show less";
        } else {
             descriptionEight.style.display = 'none' ;
+            btn8.innerHTML = "Find out more!";
         }
 });
